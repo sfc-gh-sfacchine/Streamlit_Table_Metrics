8dc8e821c20afd518376968fd2c5e8daf632b625
sfc-gh-susmani <129330134+sfc-gh-susmani@users.noreply.github.com>
2023-08-04 10:37:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import streamlit as st
 import pandas as pd
 from snowflake.snowpark.context import get_active_session
 
-def generate_time_travel_df(current_active_bytes, data_change_rate, time_periods):
-    time_travel_df = pd.DataFrame()
-    for days in time_periods:
-        time_travel_df[f'{days} days'] = current_active_bytes * (1 + data_change_rate) ** (days / 30)
-    return time_travel_df
+def generate_time_travel_df(current_active_bytes, data_change_rate):
+    time_travel_data = []
+    for days in range(1, 91):
+        time_travel_bytes = current_active_bytes * (1 + data_change_rate) ** (days / 30)
+        fail_safe_bytes = time_travel_bytes*7
+        time_travel_data.append([days, time_travel_bytes,fail_safe_bytes])
+    return pd.DataFrame(time_travel_data, columns=['day', 'time-travel bytes', 'fail_safe_bytes'])
+
 
 
 # Write directly to the app
@@ -28,16 +31,13 @@ current_active_bytes = df[df['TABLE_NAME'] == selected_table]['ACTIVE_BYTES'].va
 st.write(f'Current Active Bytes for {selected_table}: {current_active_bytes}')
 
 data_change_rate = st.number_input('Enter the expected data change rate (decimal)', min_value=0.0, max_value=1.0, value=0.1, step=0.01)
-time_periods = st.multiselect('Select time periods for analysis (in days)', list(range(1, 91)), [30, 60])
 
-if len(time_periods) > 0:
-    # Generate DataFrame with time-travel options
-    time_travel_df = generate_time_travel_df(current_active_bytes, data_change_rate, time_periods)
-    
-    # Plot bar graph using Streamlit
-    st.subheader(f'Time-Travel Scenario for {selected_table}')
-    st.bar_chart(time_travel_df)
-    
-    # Display DataFrame
-    st.write('DataFrame with Time-Travel and Fail-Safe Options:')
-    st.dataframe(time_travel_df)  
+
+# Generate DataFrame with time-travel options
+time_travel_df = generate_time_travel_df(current_active_bytes, data_change_rate)    
+# Plot bar graph using Streamlit
+st.subheader(f'Time-Travel Scenario for {selected_table}')
+st.bar_chart(time_travel_df.set_index('day'))    
+# Display DataFrame
+st.write('DataFrame with Time-Travel and Fail-Safe Options:')
+st.dataframe(time_travel_df)
